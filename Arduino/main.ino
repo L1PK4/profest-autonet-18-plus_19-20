f5ccfa975428f92daaa093e28ff131b0a37f918b
L15K4 <ostrishko.danila@gmail.com>
2019-11-23 14:08:42 +0300

    Fix2

--- Arduino/main.ino
+++ Arduino/main.ino
@@ -1,5 +1,5 @@
 #define INA1 2
-#define INB1 3 
+#define INB1 3
 #define PWM1 4
 #define INA2 5
 #define INB2 6
@@ -26,30 +26,30 @@ void setup()
 }
 
 void set_dir(bool dir) {direction = dir;}
-void set_speed(int sp) 
+void set_speed(int sp)
 {
     if(sp > 0 && sp < 256)
         speed = sp;
     else
         speed = (sp > 256)?255:0;
 }
-    
+
 String command = "";
 
 void loop()
 {
-    while(!Serial.available());    
+    while(!Serial.available());
     command = Serial.readStringUntil('\n');
 //      Parser
     speed = command[0];
     direction = (command[1] & 1)>>1;
     isturning = (command[1] & 2)>>1;
-    angle = (command[2] & 4)>>1;
+    angle = (command[1] & 4)>>1;
 //      OCHEN' LIPKA
     analogWrite(PWM1, speed);
     digitalWrite(INA1, (speed != 0)?direction:LOW);
     digitalWrite(INB1, (speed != 0)?!direction:LOW);
     analogWrite(PWM1, isturning?200:0);
-    digitalWrite(INA2, angle);
-    digitalWrite(INB2, !angle);
+    digitalWrite(INA2, isturning?angle:LOW);
+    digitalWrite(INB2, isturning?!angle:LOW);
 }
