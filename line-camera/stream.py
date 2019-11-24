0d7a7994d23df5aed666967273219c93c6a07b57
JonnhyMarlison <renat.enokaev@mail.ru>
2019-11-24 06:54:09 +0300

    fix stream.

--- line-camera/stream.py
+++ line-camera/stream.py
@@ -1,43 +1,23 @@
 import cv2
 import numpy as np
  
-# Create a VideoCapture object
-cap = cv2.VideoCapture(-1)
- 
-# Check if camera opened successfully
-if (cap.isOpened() == False): 
-  print("Unable to read camera feed")
- 
-# Default resolutions of the frame are obtained.The default resolutions are system dependent.
-# We convert the resolutions from float to integer.
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
- 
-# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+cap = cv2.VideoCapture(0)
  
 while(True):
-  ret, frame = cap.read()
- 
-  if ret == True: 
-     
-    # Write the frame into the file 'output.avi'
-    out.write(frame)
- 
-    # Display the resulting frame    
-    cv2.imshow('frame',frame)
- 
-    # Press Q on keyboard to stop recording
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
- 
-  # Break the loop
-  else:
-    break 
- 
-# When everything done, release the video capture and video write objects
+ 	ret, frame = cap.read()
+	
+	timer = cv2.getTickCount()
+	fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+	
+	cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), 
+	cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+
+	cv2.imshow("Stream", frame)
+	
+	if cv2.waitKey(1) & 0xFF == ord('q'): 
+		break
+
 cap.release()
-out.release()
- 
-# Closes all the frames
 cv2.destroyAllWindows()
+
+#python stream.py
